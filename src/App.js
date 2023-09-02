5701f44c5c6f867e946e6299433979fdb300e6b3
Ashwin Krishnan <9294975+ashwinknan@users.noreply.github.com>
2023-09-02 13:11:53 +0530

    Final Style Change

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// App.js
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -6,32 +7,31 @@ function App() {
   const meetingSlots = [
     {
       title: '15 Minute Meeting Slot',
-      description: 'Use this slot for quick catch-ups, status updates, or introductory meetings.',
+      description: 'Use this slot for quick catch-ups, FYIs or clarifications',
       link: 'https://ooud5deirh.larksuite.com/scheduler/eee033aaca05293a',
     },
     {
       title: '30 Minute Meeting Slot',
-      description: 'Ideal for detailed discussions, team meetings, or client calls that require in-depth conversations.',
+      description: 'Ideal for detailed discussions, video reviews or single agenda item discussions.',
       link: 'https://ooud5deirh.larksuite.com/scheduler/98df08de51b46ae3',
     },
     {
       title: '45 Minute Meeting Slot',
-      description: 'Perfect for brainstorming sessions, long meetings, or discussions that require ample time for detailed exploration.',
-      link: 'https://ooud5deirh.larksuite.com/scheduler/3fbe7b9f3b3e080f',
+      description: 'Perfect for brainstorming sessions, long meetings, or discussions',
     },
   ];
 
   return (
     <div className="container">
-      <h1 className="my-4">Ashwin's Meeting Calendar</h1>
+      <h1 className="my-4 text-center">Ashwin's Meeting Calendar</h1> {/* Centered title */}
       <div className="row">
         {meetingSlots.map((slot, index) => (
           <div className="col-md-4" key={index}>
-            <div className="card text-white bg-light mb-3" style={{ maxWidth: '20rem', minHeight: '10rem' }}>
+            <div className={`card custom-card d-flex flex-column`} style={{ maxWidth: '20rem', minHeight: '10rem' }}> {/* Flex styles */}
               <div className="card-header">{slot.title}</div>
-              <div className="card-body">
+              <div className="card-body d-flex flex-column"> {/* Flex styles */}
                 <p className="card-text">{slot.description}</p>
-                <a href={slot.link} className="btn btn-primary" target="_blank" rel="noopener noreferrer">
+                <a href={slot.link} className="btn btn-primary mt-auto" target="_blank" rel="noopener noreferrer"> {/* mt-auto */}
                   Book Slot
                 </a>
               </div>
@@ -44,4 +44,3 @@ function App() {
 }
 
 export default App;
-
